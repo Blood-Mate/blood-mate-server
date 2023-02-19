cd3565e0c5c71c690aaa81adac75339e18e1e4bf
yunz0926 <chldbswl9@gmail.com>
2023-02-19 20:07:20 +0900

    feat: User entity 생성

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { UserRole } from 'src/types/enums';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.GENERAL })
+  role!: UserRole;
+
+  @Column({ nullable: true })
+  refreshToken: string;
+}
---
+++ src/types/enums.ts
@@ -0,0 +1,4 @@
+export enum UserRole {
+  GENERAL = 'general',
+  GUARDIAN = 'guardian',
+}
