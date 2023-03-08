97d172f8aa7f39878368a11e2f3f80f772740982
yunz0926 <chldbswl9@gmail.com>
2023-03-08 14:37:29 +0900

    minor fix

--- src/auth/guard/jwt.strategy.ts
+++ src/auth/guard/jwt.strategy.ts
@@ -1,15 +1,11 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-import { Repository } from 'typeorm';
-import { User } from 'src/entities/user.entity';
+import { UserRepository } from 'src/repositories/user.repository';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    @InjectRepository(User) private userRepository: Repository<User>,
-  ) {
+  constructor(private readonly userRepository: UserRepository) {
     super({
       secretOrKey: process.env.JWT_SECRET_KEY,
       ignoreExpiration: false,
@@ -20,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: any): Promise<any> {
     const { id } = payload;
 
-    const user = this.userRepository.findOne({ where: { id } });
+    const user = this.userRepository.findUserById(id);
 
     if (!user) {
       throw new UnauthorizedException('User not Found');
