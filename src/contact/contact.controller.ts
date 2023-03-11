b62e10ba29e0f28a97d904539d73692b257e6d28
yunz0926 <chldbswl9@gmail.com>
2023-03-11 12:08:38 +0900

    feat: 지인 연락처 등록/조회/삭제/수정 api

---
+++ src/contact/contact.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseArrayPipe,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { AuthUser } from 'src/auth/decorator/auth-user.docrator';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { User } from 'src/entities/user.entity';
+import { ContactService } from './contact.service';
+import { AddContactDto } from './dto/add-contact.dto';
+import { ChangeSendingTargetDto } from './dto/change-sending-target.dto';
+import { ContactListDto } from './dto/contact.dto';
+
+@Controller('contact')
+export class ContactController {
+  constructor(private readonly contactService: ContactService) {}
+
+  @ApiBearerAuth()
+  @Post('/')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '지인 연락처 등록' })
+  async addContacts(
+    @Body(new ParseArrayPipe({ items: AddContactDto }))
+    contactsDto: AddContactDto[],
+    @AuthUser() user: User,
+  ) {
+    return this.contactService.addContacts(user, contactsDto);
+  }
+
+  @ApiBearerAuth()
+  @Get('/')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '지인 연락처 조회' })
+  async getContacts(@AuthUser() user: User): Promise<ContactListDto> {
+    return this.contactService.getContacts(user.id);
+  }
+
+  @ApiBearerAuth()
+  @Delete('/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '지인 연락처 삭제' })
+  async deleteContact(
+    @AuthUser() user: User,
+    @Param('id', ParseIntPipe) contactId: number,
+  ) {
+    return this.contactService.deleteContact(user.id, contactId);
+  }
+
+  @ApiBearerAuth()
+  @Patch('/')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: '알림 전송 여부 수정' })
+  async changeSendingTarget(
+    @AuthUser() user: User,
+    @Body() changeSendingTargetDto: ChangeSendingTargetDto,
+  ) {
+    return this.contactService.changeSendingTarget(
+      user.id,
+      changeSendingTargetDto,
+    );
+  }
+}
---
+++ src/repositories/contact.repository.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Contact } from 'src/entities/contact.entity';
+import { User } from 'src/entities/user.entity';
+import { DeleteResult, Repository } from 'typeorm';
+
+@Injectable()
+export class ContactRepository extends Repository<Contact> {
+  constructor(
+    @InjectRepository(Contact) private readonly repository: Repository<Contact>,
+  ) {
+    super(repository.target, repository.manager, repository.queryRunner);
+  }
+
+  async createContact(
+    user: User,
+    name: string,
+    phoneNumber: string,
+  ): Promise<void> {
+    const contact = this.repository.create({
+      user,
+      name,
+      phoneNumber,
+    });
+
+    try {
+      await this.repository.save(contact);
+    } catch (e) {
+      throw e;
+    }
+    return;
+  }
+
+  async getContacts(userId: number): Promise<Contact[]> {
+    return this.repository.find({ where: { user: { id: userId } } });
+  }
+
+  async findContactByPhoneNumber(
+    userId: number,
+    phoneNumber: string,
+  ): Promise<Contact> {
+    return this.repository.findOne({
+      where: { user: { id: userId }, phoneNumber },
+    });
+  }
+
+  async deleteContactById(
+    userId: number,
+    contactId: number,
+  ): Promise<DeleteResult> {
+    return this.repository.delete({ id: contactId, user: { id: userId } });
+  }
+
+  async updateSendingTarget(
+    userId: number,
+    contactId: number,
+    isSendingTarget: boolean,
+  ): Promise<void> {
+    const contact = await this.repository.findOne({
+      where: { id: contactId, user: { id: userId } },
+    });
+
+    contact.isSendingTarget = isSendingTarget;
+
+    try {
+      await this.repository.save(contact);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getSendingTargetContacts(userId: number): Promise<Contact[]> {
+    return this.repository.find({
+      where: {
+        user: { id: userId },
+        isSendingTarget: true,
+      },
+    });
+  }
+}
