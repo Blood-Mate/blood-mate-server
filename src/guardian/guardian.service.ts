aae8b693be9b704dce5a90c06ab2dada3c4f1c9f
yunz0926 <chldbswl9@gmail.com>
2023-03-12 11:17:53 +0900

    feat: 보호자 등록/조회/삭제 기능

---
+++ src/guardian/guardian.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { GuardianRepository } from 'src/repositories/guardian.repository';
+import { UserRepository } from 'src/repositories/user.repository';
+import { GetGuardianRequestorDto } from './dto/get-guardian-requestor.dto';
+import { GetGuardianDto } from './dto/get-guardian.dto';
+
+@Injectable()
+export class GuardianService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly guardianRepository: GuardianRepository,
+  ) {}
+  async addGuardian(user: User, counterpartId: number): Promise<void> {
+    const counterpart = await this.userRepository.findUserById(counterpartId);
+
+    return this.guardianRepository.createGuardian(user, counterpart);
+  }
+
+  async getGuardians(userId: number): Promise<GetGuardianDto[]> {
+    const guardians = await this.guardianRepository.findByRequestorId(userId);
+
+    return guardians.map((guardian) => GetGuardianDto.of(guardian));
+  }
+
+  async getGuardianRequestors(
+    userId: number,
+  ): Promise<GetGuardianRequestorDto[]> {
+    const requestors = await this.guardianRepository.findByGuardianId(userId);
+
+    return requestors.map((requestor) => GetGuardianRequestorDto.of(requestor));
+  }
+
+  async deleteGuardian(userId: number, guardianId: number): Promise<void> {
+    const result = await this.guardianRepository.deleteGuardian(
+      userId,
+      guardianId,
+    );
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`guardianId ${guardianId} does not exist`);
+    }
+  }
+}
